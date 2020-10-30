607b49161628def7c81c102f7e484eb75e39ea25
Roland Huß <rhuss@redhat.com>
2020-10-30 08:34:48 +0100

    Update creation of the broker with the corresponding kn equivalent

--- namespace-setup.sh
+++ namespace-setup.sh
@@ -9,10 +9,10 @@ read username
 echo "Creating namespace ${username}"
 oc new-project ${username}
 
-echo "Injecting default Knative broker"
-oc label namespace ${username} eventing.knative.dev/injection=enabled
+echo "Creating default Knative broker"
+kn broker create default
 
 echo "Creating a PingSource for events"
-kn source ping create my-ping --schedule "* * * * *" --data '{ "name": "PingSource" }' --sink broker:default
+kn source ping create my-ping --data '{ "name": "PingSource" }' --sink broker:default
 
 echo "Done! Your cluster is now ready for testing."
