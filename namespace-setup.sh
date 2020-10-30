b23a8590c337d099028645e8b87fd2c1d921123c
Roland Huß <rhuss@redhat.com>
2020-10-30 14:53:55 +0100

    adapted to work also with kubernetes (#25)

--- namespace-setup.sh
+++ namespace-setup.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 echo "Hello, and thank you for agreeing to participate in Functions Test Day!
 Please provide your Red Hat username, and a namespace will be created and configured for you."
@@ -7,12 +7,12 @@ echo -n "username: "
 read username
 
 echo "Creating namespace ${username}"
-oc new-project ${username}
+kubectl create ns ${username}
 
 echo "Creating default Knative broker"
-kn broker create default
+kn broker create default -n ${username}
 
 echo "Creating a PingSource for events"
-kn source ping create my-ping --data '{ "name": "PingSource" }' --sink broker:default
+kn source ping create my-ping --data '{ "name": "PingSource" }' --sink broker:default -n ${username}
 
 echo "Done! Your cluster is now ready for testing."
